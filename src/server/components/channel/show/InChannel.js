2422ea4f6d57017d85afdc4ee26fa7abeb255679
Deni <54061556+DenSpec@users.noreply.github.com>
2021-11-21 22:34:47 +0100

    Update InChannel.js

--- src/server/components/channel/show/InChannel.js
+++ src/server/components/channel/show/InChannel.js
@@ -1,63 +1,19 @@
 // Fonctionnalité // 
-import React, {useRef} from 'react'
-import { DiscordMention, DiscordMessage, DiscordMessages } from '@danktuary/react-discord-message';
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollection } from "react-firebase-hooks/firestore"
-import 'firebase/compat/auth';
-import { auth } from "../../../../auth/backend/database"
-import firebase from "firebase/compat/app";
-import { useSelector } from "react-redux";
-import { selectServerId, selectServerName} from "../../../../store/ServerSlice";
+import React from 'react'
 import Sidebar from '../../UI/Sidebar/Sidebar'
 import ChannelList from '../list/ChannelList'
 import ChannelChat from '../chat/ChannelChat'
-// Firebase
-import { selectChannelId, selectChannelName } from  '../../../../store/ChannelSlice'
+/* C'est une simple fonction pour pouvoir regroupé tous les éléments. */
 const InChannel = () => { 
-  const db = firebase.firestore()
-  const [server] = useCollection(db.collection("server"));
-  const serverId = useSelector(selectServerId);
-  const channelId = useSelector(selectChannelId);
-  const channelName = useSelector(selectChannelName);
-  const [user] = useAuthState(auth);
-  const inputRef = useRef("");
-  const chatRef = useRef(null);
-  const [messages] = useCollection(
-    channelId &&
-      db
-      .collection("server")
-      .doc(serverId)
-      .collection("channel")
-      .doc(channelId)
-      .collection("messages")
-      .orderBy("timestamp", "asc")
-  );
-
-  const sendMessage = (e) => {
-    e.preventDefault();
-
-    if (inputRef.current.value !== "") {
-      db.collection("server")
-      .doc(serverId)
-      .collection("channel")
-      .doc(channelId)
-      .collection("messages").add({
-        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-        message: inputRef.current.value,
-        name: user?.displayName,
-        photoURL: user?.photoURL,
-    });
-}
-};
-
     return (
 
 <>
+
 <Sidebar/>
 <ChannelList/>
 <ChannelChat/>
 
-    </>
+</>
 );
 };
 
