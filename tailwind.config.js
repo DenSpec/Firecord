82c54aa521ee257e4ea58cb07ad7ee438bd49a70
Deni <54061556+DenSpec@users.noreply.github.com>
2021-12-05 15:37:47 +0100

    public

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  mode: 'jit',
+  enabled: true,
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'media', // class, 'media' or boolean
+  theme: {
+    cursor: {
+      auto: 'auto',
+      default: 'default',
+      pointer: 'pointer',
+ },      
+    extend: {
+      colors: {
+      discord_channelsBg: '#2f3136' 
+      },
+        gray: {
+          900: '#202225',
+          800: '#2f3136',
+          700: '#36393f',
+          600: '#4f545c',
+          400: '#d4d7dc',
+          300: '#e3e5e8',
+          200: '#ebedef',
+          100: '#f2f3f5',
+        },
+      
+      spacing: {
+        88: '22rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
